9a306f98b150e8953c2e1cc8d98532770771cf18
Ricardo <ricardo.lyon@globant.com>
2016-01-25 13:29:59 -0600

    objects

--- app/objects.js
+++ app/objects.js
@@ -2,7 +2,12 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.objectsAnswers =  {
   alterContext : function(fn, obj) {
+    this.name = obj.name;
+    this.greeting = obj.greeting;
 
+    this.sayIt = fn;
+
+    return this.sayIt.call(this);
   },
 
   alterObjects : function(constructor, greeting) {
@@ -10,6 +15,13 @@ exports.objectsAnswers =  {
   },
 
   iterate : function(obj) {
+    var result = [];
+    var props = Object.getOwnPropertyNames(obj);
 
+    props.forEach((property) => {
+      result.push(property + ': ' + obj[property]);
+    });
+
+    return result;
   }
 };
